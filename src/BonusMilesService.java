6bc36e4fd81bc33ebc87c13e2f081b4a2677b6d6
Ксения Сажнева <13pokemon@mail.ru>
2022-05-19 17:51:25 +0300

    Fixed the calculate method

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
-    public int calculate(int price) {
-        int bonus = price / 20;
-        System.out.println("Сумма покупки: " + price + " руб.");
+    public int calculate(int cost) {
+        int bonus = cost / 20;
+        System.out.println("Сумма покупки: " + cost + " руб.");
         System.out.println("Ваш бонус: " + bonus + " миль");
         return bonus;
     }
